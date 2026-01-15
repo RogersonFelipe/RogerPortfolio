5c4e71582b0af0961ea196fa073a4d2906cacd79
RogersonFelipe <rogersonfilipi@hotmail.com>
2026-01-15 16:17:36 -0300

    ajuste API

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/github/repos', [GitHubController::class, 'repos']);
+Route::get('/github/repos', [GithubController::class, 'repos']);
