2e18623c5a5b96cd27c2d255ea6635f51cd5c63a
RogersonFelipe <rogersonfilipi@hotmail.com>
2026-02-02 12:37:41 -0300

    Ajuste:Api

--- app/Http/Controllers/GitHubController.php
+++ app/Http/Controllers/GitHubController.php
@@ -2,15 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Services\GitHubService;
+use Illuminate\Support\Facades\Http;
 
-class GitHubController extends Controller
+class GithubController extends Controller
 {
-    public function repos(GitHubService $gitHubService)
+    public function repos()
     {
-        return response()->json(
-            $gitHubService->getRepositories()
-        );
+        $username = env('GITHUB_USERNAME');
+        $token = env('GITHUB_TOKEN');
+
+        if (!$username) {
+            return response()->json([
+                'error' => 'GITHUB_USERNAME não configurado'
+            ], 500);
+        }
+
+        $response = Http::withHeaders([
+            'Accept' => 'application/vnd.github+json',
+            'Authorization' => $token ? "Bearer {$token}" : null,
+        ])->get("https://api.github.com/users/{$username}/repos");
+
+        if ($response->failed()) {
+            return response()->json([
+                'error' => 'Erro ao buscar repositórios',
+                'github_response' => $response->json(),
+            ], $response->status());
+        }
+
+        return response()->json($response->json());
     }
 }
